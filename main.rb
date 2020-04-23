3f5ef668bc7bf2a0e83791a0e026539d553aa976
Rossiel Carranza <rossielcarranza@gmail.com>
2020-04-23 17:51:20 -0300

    Add players moves
    - Turn for players
    - frozen string literal

--- main.rb
+++ main.rb
@@ -1,18 +1,39 @@
+# frozen_string_literal: true
+
 puts 'Welcome to the game!'
 puts 'This game is designed for two players'
 puts 'Player one, what\'s your name?'
 name_one = gets.chomp
 puts "#{name_one} welcome aboard!"
+
 puts 'Player two, what\'s your name?'
 name_two = gets.chomp
 puts "#{name_two} welcome aboard!"
+
 puts "#{name_one} would be the X and will make the first move"
 puts "#{name_two} would be the O and will make the second move"
+puts 'This is the board of the game'
 row = ['     |     |     ']
 puts row
 puts separator = '-----------------'
 puts row
 puts separator
 puts row
+
 puts 'Let\'s start!'
 puts "#{name_one} it's your turn"
+puts row
+puts separator = '-----------------'
+puts row
+puts separator
+puts row
+
+puts "#{name_two} it's your turn"
+puts row
+puts separator = '-----------------'
+puts row
+puts separator
+puts row
+
+# It's going to be a maximum of nine movements for the game.
+# We decided to add two just movements for representation.
