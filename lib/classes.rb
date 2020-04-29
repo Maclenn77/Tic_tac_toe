9411bbc1d2b469c60003f7975cd6f1edd947d432
Maclenn77 <maclenn77@gmail.com>
2020-04-29 16:07:03 -0500

    Added check_winner to loop_match. Doing a loop for keep playing

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_accessor :name, :piece, :score  
+  attr_accessor :name, :piece, :score
 
   def initialize(name, piece = ' ', score = 0)
     @name = name
@@ -21,13 +21,14 @@ class Player
 end
 
 class Match
-  attr_accessor :hash
+  attr_accessor :hash, :result
   attr_reader :turn
 
   def initialize(matches = 0)
     @hash = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
     @turn = 0
     @matches = matches
+    @result = ''
   end
 
   def who_is_first?(player_one, player_two)
@@ -53,59 +54,53 @@ class Match
   end
 
   def next_turn(*args)
-    @turn += 1
     player_turn = args[0..1].detect { |a| a.piece == 'X' } if @turn.odd?
     player_turn = args[0..1].detect { |a| a.piece == 'O' } if @turn.even?
     player_turn
-    # check_winner(player_turn, player_next, puts_hash, board_hash) if @turn >= 5
-    # puts_hash['your_turn'].call(player_next.name) if @turn < 9
-    
-    # puts_hash['draw'].call
-    # keep_playing?(player_turn, player_next, puts_hash, board_hash)
+  end
+
+  def add_turn
+    @turn += 1
   end
 
   def place_move(move, player_one)
     @hash[move] = player_one.piece
   end
 
-  def check_winner(player_turn, player_next, puts_hash, board_hash)
+  def check_winner(player_turn)
     winner = false
     winner_cases = [[@hash[1], @hash[2], @hash[3]], [@hash[1], @hash[4], @hash[7]],
                     [@hash[1], @hash[5], @hash[9]], [@hash[2], @hash[5], @hash[8]],
                     [@hash[3], @hash[6], @hash[9]], [@hash[3], @hash[5], @hash[7]],
                     [@hash[4], @hash[5], @hash[6]], [@hash[7], @hash[8], @hash[9]]]
     winner_cases.each { |cases| winner = true if cases.all?(player_turn.piece) }
-    if winner
-      puts_hash['you_won'].call(player_turn.name)
-      player_turn.change_score
-      keep_playing?(player_turn, player_next, puts_hash, board_hash)
-    end
     winner
   end
 
-  def keep_playing?(player_one, player_two, puts_hash, board_hash)
-    condition = false
-    puts_hash['play_again'].call
-    choice = gets.chomp
-    until condition
-      if choice == 'y'
-        condition = true
-        restart_match(player_one, player_two, puts_hash, board_hash)
-      elsif choice == 'n'
-        puts_hash['exit_match'].call
-        total_score(player_one, player_two)
-        exit
-      else
-        puts_hash['valid_op'].call
-        choice = gets.chomp
-      end
-    end
-  end
+  # def keep_playing?(player_one, player_two, puts_hash, board_hash)
+  #   condition = false
+  #   puts_hash['play_again'].call
+  #   choice = gets.chomp
+  #   until condition
+  #     if choice == 'y'
+  #       condition = true
+  #       restart_match(player_one, player_two, puts_hash, board_hash)
+  #     elsif choice == 'n'
+  #       puts_hash['exit_match'].call
+  #       total_score(player_one, player_two)
+  #       exit
+  #     else
+  #       puts_hash['valid_op'].call
+  #       choice = gets.chomp
+  #     end
+  #   end
+  # end
 
   def total_score(player_one, player_two, matches)
-    puts "Total matches: #{matches} Total Score:\n
+    score = "Total matches: #{matches} Total Score:\n
           #{player_one.name}: #{player_one.score}\n
           #{player_two.name}: #{player_two.score}"
+    score
   end
 
   def restart_match(player_one, player_two, puts_hash, board_hash)
