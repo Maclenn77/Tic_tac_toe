22ada0cedf0625a89d4060ca9f0f4322e066c67f
Maclenn77 <maclenn77@gmail.com>
2020-05-04 15:12:41 -0500

    Created a spec for who_is_first? Method from Match

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -1,1 +1,17 @@
 require './lib/match'
+
+describe Match do
+  subject(:match) { Match.new }
+  subject(:player_one) { Player.new('Jerry') }
+  subject(:player_two) { Player.new('Bob') }
+
+  describe '#who_is_first' do
+    it 'Pick a piece for players' do
+      expect { match.who_is_first?(player_one, player_two) }.to change { player_one.piece }.from(' ').to(/[XO]/)
+    end
+
+    it 'Pick a piece for players' do
+      expect { match.who_is_first?(player_one, player_two) }.to change { player_two.piece }.from(' ').to(/[XO]/)
+    end
+  end
+end
