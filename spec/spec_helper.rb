27f9c89c9e18057d0cae50c7aaa642aeec713c98
Maclenn77 <maclenn77@gmail.com>
2020-05-04 18:48:23 -0500

    Uncommented previous ln 46

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,23 +78,22 @@ RSpec.configure do |config|
     # unless a formatter has already been configured
     # (e.g. via a command-line flag).
 #    config.default_formatter = "doc"
-  end
+  #end
 
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  #config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = :random
+  #config.order = :random
 
   # Seed global randomization in this process using the `--seed` CLI option.
   # Setting this allows you to use `--seed` to deterministically reproduce
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
-  Kernel.srand config.seed
-=end
+  #Kernel.srand config.seed
 end
