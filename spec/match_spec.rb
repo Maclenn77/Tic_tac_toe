265a1c4f2993807f4712248d227609fa02cdf149
Rossiel Carranza <rossielcarranza@gmail.com>
2020-05-04 17:33:13 -0300

    Add specs for match methods
    - next_turn
    - add_turn

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -14,4 +14,22 @@ describe Match do
       expect { match.who_is_first?(player_one, player_two) }.to change { player_two.piece }.from(' ').to(/[XO]/)
     end
   end
+
+=begin
+  describe '#next_turn' do
+    it 'returns the player with the piece \'X\' if turn is odd' do
+      expect(match.next_turn(player_one, player_two)).to eql(player_one)
+    end
+
+    it 'returns the player with the piece \'O\' if turn is even' do
+      expect(match.next_turn(player_one, player_two)).to eql(player_two)
+    end
+  end
+=end
+
+  describe '#add_turn' do
+    it 'inscrements the value of turn by 1' do
+      expect { match.add_turn }.to change { match.turn }.from(0).to(1)
+    end
+  end
 end
