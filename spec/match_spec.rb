6b7bcb9b06483de599eb2c88aac7d08cde4d58ca
Maclenn77 <maclenn77@gmail.com>
2020-05-04 15:58:50 -0500

    Created specs for place_move and check_winner'

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -18,7 +18,9 @@ describe Match do
 =begin
   describe '#next_turn' do
     it 'returns the player with the piece \'X\' if turn is odd' do
-      expect(match.next_turn(player_one, player_two)).to eql(player_one)
+    consider ->  match.who_is_first?(players...)
+
+no setting piece  ->    expect(match.next_turn(player_one, player_two)).to eql(player_one)
     end
 
     it 'returns the player with the piece \'O\' if turn is even' do
@@ -30,6 +32,29 @@ describe Match do
   describe '#add_turn' do
     it 'inscrements the value of turn by 1' do
       expect { match.add_turn }.to change { match.turn }.from(0).to(1)
+    end
+  end
+
+  describe '#place_move' do
+    it 'changes the hash with player\'s piece' do
+      expect { match.place_move(1, player_one) }.to change { match.hash[1] }.from(1).to(player_one.piece)
+    end
+  end
+
+  describe '#check_winner' do
+    it 'returns false when there\'s not a winner' do
+      match.place_move(1, player_two)
+      match.place_move(2, player_one)
+      match.place_move(9, player_two)
+      match.place_move(4, player_one)
+      expect(match.check_winner(player_one)).to eql(false)
+    end
+
+    it 'returns true when there\'s a winner' do
+      match.place_move(1, player_one)
+      match.place_move(4, player_one)
+      match.place_move(7, player_one)
+      expect(match.check_winner(player_one)).to eql(true)
     end
   end
 end
